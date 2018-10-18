332a9ee0ac31d048fff6f09b635a2734936d01cf
Radu <radu103@hotmail.com>
2018-10-18 17:49:36 +0300

    image croped in 6 horizontal sections

--- app.js
+++ app.js
@@ -17,11 +17,41 @@ setInterval(function(){
 				console.log(err);
 			}
 			
-			im.convertGrayscale();
+			var width = im.width();
+			var height = im.height();
 			
-			var contours = im.findContours();
-			console.log(contours);
-			console.log(' ');
+			if (width < 1 || height < 1) 
+				throw new Error('Image has no size');
+
+			var sectorWidth = Math.round(width / 6);
+			
+			for(var i = 1; i <= 6; i++)
+			{
+				let x1 = (i-1) * sectorWidth;
+				let croppedImg = im.crop(x1, 0, sectorWidth - 1, height);
+				
+				let cropName = folder + name + "_c" + i + '.jpeg';
+				croppedImg.save(cropName);
+				
+				var im_canny = croppedImg.copy();
+
+				var lowThresh = 0;
+				var highThresh = 100;
+				var nIters = 1;
+
+				im_canny.canny(lowThresh, highThresh);
+				im_canny.dilate(nIters);
+			  
+				var contours = im_canny.findContours();
+
+				var size = contours.size();
+				var cars = Math.round(size / 4);
+				
+				console.log("Cars " + i + " : ", cars);
+				
+				var fileName2 = folder + name + "_" + i + '_canny.jpeg';
+				im_canny.save(fileName2);
+			}
 		});
 	}); 
 	
